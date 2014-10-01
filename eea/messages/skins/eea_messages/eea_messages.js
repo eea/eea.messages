86e2fdcc66bdc6223ca818b7e81339ecfa5511ab
ichim-david <ichim.david@gmail.com>
2014-10-01 16:53:55 +0300

    Refs #20970; Changed the following:
    - Removed permissions.zcml
    - Added cssregistry and jsregistry
    - Removed five registry of package

--- eea/messages/skins/eea_messages/eea_messages.js
+++ eea/messages/skins/eea_messages/eea_messages.js
@@ -1,3 +1,5 @@
 jQuery(function($){
-   var $holder = $("<div id='messages-folder'/>");
+   var $holder = $("<div id='eea-messages-holder'/>");
+    $holder.appendTo("body");
+    $(".portalMessages").appendTo($holder);
 });
