eb45b1f0a7a569bf2bb1d717ab159dc8808dde2c
ichim-david <ichim.david@gmail.com>
2014-10-08 18:04:06 +0300

    Refs #20970; added jquery knobs to the portal messages; hide after 15sec and pause on hover

--- eea/messages/skins/eea_messages/eea_messages.js
+++ eea/messages/skins/eea_messages/eea_messages.js
@@ -47,15 +47,19 @@ EEAMessages.Message.prototype = {
             self.context.trigger('EEAMessages.hidden', self);
         });
 
-        self.context.bind('EEAMessages.display', function(evt, data){
-            self.handle_message_display(data);
-            self.context.trigger('EEAMessages.displayed', self);
+        self.context.bind('EEAMessages.render', function(evt, data){
+            self.handle_message_render(data);
+            self.context.trigger('EEAMessages.rendered', self);
         });
+    },
+    _subscribeEvents: function() {
+
     },
     initialize: function(){
         var self = this;
         self._build();
         self._bindEvents();
+        self._subscribeEvents();
         // trigger some events
         self.context.trigger('EEAMessages.add', self);
         return self;
@@ -74,27 +78,47 @@ EEAMessages.Message.prototype = {
         if (linkFound) {
             $elem.trigger('EEAMessages.ActionMessage', self);
         }
-        $elem.trigger('EEAMessages.display', self);
+        $elem.trigger('EEAMessages.render', self);
     },
 
-    handle_message_display: function(data) {
+    handle_message_render: function(data) {
         var self = data;
-        var timeoutID = window.setTimeout(
-            $.proxy(self.handle_message_hide, self), self.settings.timeout);
-        self.context.mouseenter($.proxy(function(ev){
-            console.log('entered');
-            window.clearTimeout(timeoutID);
-        }, data));
-        self.context.mouseleave($.proxy(function(ev){
-            timeoutID = window.setTimeout(
-                $.proxy(self.handle_message_hide, self), self.settings.timeout);
-            console.log('leaved');
-        }, data));
+        var $knob = $("<input class='knob'/>").appendTo(self.context);
+        $knob.knob({
+            value: 0,
+            'readOnly': true,
+            'width': 30,
+            'height': 30,
+            'background': 'white',
+            'dynamicDraw': true,
+            'thickness': 0.1,
+            'tickColorizeValues': true,
+            'skin': 'tron'
+        });
+        var count = 15;
+        var animateKnob = function() {
+            $knob.val(count).trigger('change');
+            var $parent = $knob.closest('div');
+            if($parent.css('display') === "inline") {
+                $parent.css('display', 'block');
+            }
+            count -= 1;
+            if (count <= 0) {
+               window.clearInterval(intervalID);
+                self.context.fadeOut();
+            }
+        };
+        var intervalID = window.setInterval(animateKnob, 1000);
+        self.context.hover(function(){
+             window.clearInterval(intervalID);
+        }, function() {
+            intervalID = window.setInterval(animateKnob, 1000);
+        });
     },
 
     handle_message_hide: function(data){
         var self = data || this;
-        self.context.hide();
+        self.context.fadeOut();
     }
 };
 
