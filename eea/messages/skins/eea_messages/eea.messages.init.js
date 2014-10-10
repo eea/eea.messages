352ece525e93f901720beb54d5526df3d41c7fe4
ichim-david <ichim.david@gmail.com>
2014-10-10 17:06:48 +0300

    Refs #20970; moved initialization of eea.message to eea.messages.init.js, if other inits are needed than we can just disable the init file without customizing the original logic

---
+++ eea/messages/skins/eea_messages/eea.messages.init.js
@@ -0,0 +1,8 @@
+// Call eea.messages for all portalMessages which are not within content-core
+jQuery(function($) {
+    var messages = $('.portalMessage').filter(function() {
+        return $(this).closest('#content-core').length ? false : true;
+    });
+    messages.EEAMessages();
+});
+
--- eea/messages/skins/eea_messages/eea_messages.js
+++ eea/messages/skins/eea_messages/eea_messages.js
@@ -160,11 +160,3 @@ jQuery.fn.EEAMessages.options = {
     highThreshold: 'green'
 };
 
-// Call it
-jQuery(function($) {
-    var messages = $('.portalMessage').filter(function() {
-        return $(this).closest('#content-core').length ? false : true;
-    });
-    messages.EEAMessages();
-});
-
