7fbdc2412ceb917f1e404f903c34ede4a2bdea47
ichim-david <ichim.david@gmail.com>
2014-10-10 12:06:28 +0300

    Refs #20970; only move the portalMessages which are not part of content-core since we can apply these classes within text fields withtinyMCE

--- eea/messages/skins/eea_messages/eea_messages.js
+++ eea/messages/skins/eea_messages/eea_messages.js
@@ -162,6 +162,9 @@ jQuery.fn.EEAMessages.options = {
 
 // Call it
 jQuery(function($) {
-    $('.portalMessage').EEAMessages();
+    var messages = $('.portalMessage').filter(function() {
+        return $(this).closest('#content-core').length ? false : true;
+    });
+    messages.EEAMessages();
 });
 
