e7ffb524eada59ec8b1b4b92d0041ab3489f04fa
ichim-david <ichim.david@gmail.com>
2014-10-10 17:29:18 +0300

    Refs #20970; updated uninstall profile with empty files in order not to forget to complete unistall profile; Moved eea.messages.view to controlpanel and renamed it to eea.messages.settings

--- eea/messages/controlpanel/settings.py
+++ eea/messages/controlpanel/settings.py
@@ -1,13 +1,30 @@
 """ Control Panel
 """
+import json
+
 from zope.component import queryUtility
 from zope.interface import implements
-from eea.messages.interfaces import ISettings
-from eea.messages.config import EEAMessageFactory as _
 from plone.app.controlpanel.form import ControlPanelForm
-from plone.registry.interfaces import IRegistry
 from Products.CMFDefault.formlib.schema import SchemaAdapterBase
 from zope.formlib import form
+from Products.Five.browser import BrowserView
+from plone.registry.interfaces import IRegistry
+from zope.component import getUtility
+
+from eea.messages.config import EEAMessageFactory as _
+from eea.cache import cache
+from eea.messages.controlpanel.interfaces import ISettings
+
+
+def jsonify(obj, response=None, status=None):
+    """ Convert obj to JSON
+    """
+    if response:
+        response.setHeader("Content-type", "application/json")
+        if status:
+            response.setStatus(status)
+    return json.dumps(obj)
+
 
 class ControlPanel(ControlPanelForm):
     """ API
@@ -16,6 +33,7 @@ class ControlPanel(ControlPanelForm):
     label = _(u"EEA Messages Settings")
     description = _(u"EEA Messages settings")
     form_name = _(u"EEA Messages settings")
+
 
 class ControlPanelAdapter(SchemaAdapterBase):
     """ Form adapter
@@ -60,3 +78,25 @@ class ControlPanelAdapter(SchemaAdapterBase):
         """ Set Action Messages timeout
         """
         self.settings.timeout_action = value
+
+
+class SettingsJSONView(BrowserView):
+    """ Custom view controller
+    """
+    @cache(get_key=lambda method, self: method.__name__)
+    def messages_settings(self):
+        """
+        :return: JSON output of EEA Messages settings
+        :rtype: dict
+        """
+        registry = getUtility(IRegistry)
+        records = registry.records
+        reg_name = ISettings.__identifier__
+        values = records.values(reg_name + ".", reg_name + "0")
+        output = {}
+        for value in values:
+            output[value.fieldName] = value.value
+        return jsonify(output, self.request.response)
+
+    def __call__(self):
+        return self.messages_settings()
--- eea/messages/skins/eea_messages/eea.messages.init.js
+++ eea/messages/skins/eea_messages/eea.messages.init.js
@@ -3,7 +3,10 @@ jQuery(function($) {
     var messages = $('.portalMessage').filter(function() {
         return $(this).closest('#content-core').length ? false : true;
     });
+    debugger;
+    $.getJSON('/eea.messages.settings').done(function(json){
+       messages.EEAMessages(JSON.parse(json));
+    });
 
-    messages.EEAMessages();
 });
 
